f0f0fefe9e6e39e3c19efab676ebc6f2763b34dc
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-02-14 15:07:03 +0900

    カメラに注視スクリプトを追加、カメラがトップビューになるように調整

---
+++ unity_bomberman/Assets/Bomberman/Scripts/Game/Camera/GameMainCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMainCamera : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("MainCamera Start");
+		// 注視点設定
+        transform.LookAt(LookPosition);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public Vector3 LookPosition;
+}
