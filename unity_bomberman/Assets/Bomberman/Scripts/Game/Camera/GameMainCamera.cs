bf43eb57d4e05f7bb7e68c4c01118e247d393d77
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-02-25 16:30:16 +0900

    プレイヤーのスクリプトを追加、カメラとプレイヤーのスクリプトにnamespaceを追加

--- unity_bomberman/Assets/Bomberman/Scripts/Game/Camera/GameMainCamera.cs
+++ unity_bomberman/Assets/Bomberman/Scripts/Game/Camera/GameMainCamera.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMainCamera : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
-        Debug.Log("MainCamera Start");
-		// 注視点設定
-        transform.LookAt(LookPosition);
-    }
+namespace Bomberman {
+	public class GameMainCamera : MonoBehaviour {
+		// Start is called before the first frame update
+		void Start() {
+			Debug.Log("MainCamera Start");
+			// 注視点設定
+			transform.LookAt(LookPosition);
+		}
 
-    // Update is called once per frame
-    void Update() {
-        
-    }
+		// Update is called once per frame
+		void Update() {
 
-    public Vector3 LookPosition;
+		}
+
+		public Vector3 LookPosition;
+	}
 }
