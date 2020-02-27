54a2364019302840b7f059f7a2cd2af4283772b0
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-02-27 23:08:16 +0900

    Revert "爆発後に消滅するように修正"
    This reverts commit b46c8e508f3d8c680f30f64daacbef72746fe86b.

--- unity_bomberman/Assets/Bomberman/Scripts/Game/Bomb/Bomb001.cs
+++ unity_bomberman/Assets/Bomberman/Scripts/Game/Bomb/Bomb001.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bomb001 : MonoBehaviour {
+public class Bomb001 : MonoBehaviour
+{
     // Start is called before the first frame update
     void Start() {
         Debug.Log("Bomb001 Start");
-		// 3秒後に消滅
-        StartCoroutine(ExecuteExplosion(3.0f));
+        StartCoroutine(ExecuteExplosion(3.5f));
     }
 
     // Update is called once per frame
@@ -17,7 +17,5 @@ public class Bomb001 : MonoBehaviour {
 	private IEnumerator ExecuteExplosion(float waitTime) {
         yield return new WaitForSeconds(waitTime);
         Debug.Log("Explosion");
-		// 爆発後消滅する
-        Destroy(this.gameObject);
 	}
 }
