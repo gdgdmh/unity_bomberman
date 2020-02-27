4c9db660819642bd34547af856a71037464fa797
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-02-27 23:49:43 +0900

    爆弾が爆発した時にエフェクトが出るように追加

--- unity_bomberman/Assets/Bomberman/Scripts/Game/Bomb/Bomb001.cs
+++ unity_bomberman/Assets/Bomberman/Scripts/Game/Bomb/Bomb001.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bomb001 : MonoBehaviour
-{
+public class Bomb001 : MonoBehaviour {
+
+    public static readonly string ExplosionEffectPrefabPath = "Prefabs/Explosion001";
+
     // Start is called before the first frame update
     void Start() {
         Debug.Log("Bomb001 Start");
@@ -19,5 +21,10 @@ public class Bomb001 : MonoBehaviour
         Debug.Log("Explosion");
 		// 爆発したら消滅
         Destroy(this.gameObject);
+		// 爆発エフェクトを生成
+        GameObject effect = Resources.Load(ExplosionEffectPrefabPath) as GameObject;
+        Instantiate(effect, new Vector3(0, 0, 0), Quaternion.identity);
 	}
+
+
 }
