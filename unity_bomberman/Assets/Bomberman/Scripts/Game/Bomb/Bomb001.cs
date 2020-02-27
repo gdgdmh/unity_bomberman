d4c1df3965f1b508188a03c5e43cb62198224695
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-02-27 23:15:52 +0900

    爆発した時に消滅するように修正

--- unity_bomberman/Assets/Bomberman/Scripts/Game/Bomb/Bomb001.cs
+++ unity_bomberman/Assets/Bomberman/Scripts/Game/Bomb/Bomb001.cs
@@ -7,7 +7,7 @@ public class Bomb001 : MonoBehaviour
     // Start is called before the first frame update
     void Start() {
         Debug.Log("Bomb001 Start");
-        StartCoroutine(ExecuteExplosion(3.5f));
+        StartCoroutine(ExecuteExplosion(3.0f));
     }
 
     // Update is called once per frame
@@ -17,5 +17,7 @@ public class Bomb001 : MonoBehaviour
 	private IEnumerator ExecuteExplosion(float waitTime) {
         yield return new WaitForSeconds(waitTime);
         Debug.Log("Explosion");
+		// 爆発したら消滅
+        Destroy(this.gameObject);
 	}
 }
