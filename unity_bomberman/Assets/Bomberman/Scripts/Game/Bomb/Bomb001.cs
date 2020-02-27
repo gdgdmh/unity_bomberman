39170f29e07b0cfa68ec411a2b91220b9e458024
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-02-27 19:32:49 +0900

    爆弾のスクリプトを追加

---
+++ unity_bomberman/Assets/Bomberman/Scripts/Game/Bomb/Bomb001.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb001 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Bomb001 Start");
+        StartCoroutine(ExecuteExplosion(3.5f));
+    }
+
+    // Update is called once per frame
+    void Update() {        
+    }
+
+	private IEnumerator ExecuteExplosion(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        Debug.Log("Explosion");
+	}
+}
