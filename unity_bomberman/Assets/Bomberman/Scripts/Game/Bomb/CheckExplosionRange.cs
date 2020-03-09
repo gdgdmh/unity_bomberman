cab427353fa8bbd9c924f0ae6c041418abb07adf
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-03-10 00:22:33 +0900

    爆発範囲のチェックスクリプトの上方向チェックを仮実装

--- unity_bomberman/Assets/Bomberman/Scripts/Game/Bomb/CheckExplosionRange.cs
+++ unity_bomberman/Assets/Bomberman/Scripts/Game/Bomb/CheckExplosionRange.cs
@@ -20,12 +20,76 @@ public class CheckExplosionRange {
 	/// <summary>
 	/// 指定した方向の爆発範囲を取得する
 	/// </summary>
-	/// <param name="explosionPosition">爆発範囲</param>
+	/// <param name="explosionPosition">爆発可能な位置(複数)</param>
 	/// <param name="direction">方向</param>
-	/// <param name="range">爆発距離</param>
-	/// <param name="position">基準位置</param>
-	public static void GetDirectionRange(out Vector3[] explosionPosition, Direction direction, int range, Vector3 position) {
-		explosionPosition = new Vector3[1]; // 仮
+	/// <param name="position">基準位置(マス中央)</param>
+	/// <param name="blockOffset">ブロックの間隔(x,z)</param>
+	/// <param name="explosionRange"></param>
+	/// <param name="blockRange"></param>
+	public static void GetDirectionRange(out Vector3[] explosionPosition, Direction direction, Vector3 position, Vector3 blockOffset, Vector3 explosionRange, int blockRange) {
+		explosionPosition = new Vector3[0];
+
+		Vector3 directionVector = Vector3.zero;
+		float baseDistance = 2.0f;
+		switch (direction) {
+			case Direction.Up:
+				directionVector = Vector3.back;
+				break;
+			case Direction.Down:
+				directionVector = Vector3.forward;
+				break;
+			case Direction.Left:
+				directionVector = Vector3.right;
+				break;
+			case Direction.Right:
+				directionVector = Vector3.left;
+				break;
+			default:
+				break;
+		}
+
+		int arrayCount = 1;
+		Vector3 basePosition = position;
+		for (int i = 0; i < blockRange; ++i) {
+
+			// 爆発判定(Rayから特定レイヤーのオブジェクトがあるかによって判断)
+			LayerMask mask = LayerMask.NameToLayer("indestructible_block");
+			float maxDistance = (i + 1) * baseDistance; // マスの中央までの距離
+
+			if (Physics.Raycast(basePosition, directionVector, maxDistance, mask)) {
+				Debug.Log("hit");
+
+				return;
+			} else {
+				Debug.Log("not hit");
+				// 追加
+				System.Array.Resize(ref explosionPosition, arrayCount);
+
+				Vector3 outPosition = basePosition;
+				switch (direction) {
+					case Direction.Up:
+						// z-
+						outPosition.z -= maxDistance;
+						break;
+					case Direction.Down:
+						// z+
+						outPosition.z += maxDistance;
+						break;
+					case Direction.Left:
+						// x+
+						outPosition.x += maxDistance;
+						break;
+					case Direction.Right:
+						// x-
+						outPosition.x -= maxDistance;
+						break;
+					default:
+						break;
+				}
+				explosionPosition[arrayCount - 1] = outPosition;
+				++arrayCount;
+			}
+		}
 	}
 
 	public static void GetRange(out Vector3[] upExplosionPosition, out Vector3[] downExplosionPosition, out Vector3[] leftExplosionPosition, out Vector3[] rightExplosionPosition, int upRange, int downRange, int leftRange, int rightRange, Vector3 position) {
--- unity_bomberman/Assets/Bomberman/Scripts/Game/Task/GameMainTask.cs
+++ unity_bomberman/Assets/Bomberman/Scripts/Game/Task/GameMainTask.cs
@@ -8,9 +8,51 @@ using UnityEngine;
 public class GameMainTask : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
+        //out Vector3[] explosionPosition, Direction direction, Vector3 position, Vector3 blockOffset, Vector3 explosionRange, int blockRange
+
+        Vector3[] explosionPosition;
+        CheckExplosionRange.Direction direction = CheckExplosionRange.Direction.Up;
+        Vector3 position = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 blockOffset = new Vector3(2.0f, 2.0f, 2.0f); // ブロックの間隔は2
+        Vector3 explosionRange = new Vector3(1.9f, 1.9f, 1.9f); // ぴったりだとギリギリ当たる可能性があるので-0.1する
+        int blockRange = 2; // 2ブロック先まで届く
+        CheckExplosionRange.GetDirectionRange(out explosionPosition, direction, position, blockOffset, explosionRange, blockRange);
+
+		if (explosionPosition != null) {
+			if (explosionPosition.Length == 0) {
+                Debug.Log("pos empty");
+			}
+			foreach (Vector3 outPosition in explosionPosition) {
+                Debug.Log(string.Format("pos x={0} y={1} z={2}", outPosition.x, outPosition.y, outPosition.z));
+			}
+		} else {
+            Debug.Log("pos null");
+		}
+
     }
 
     // Update is called once per frame
     void Update() {
+		if (Input.GetKeyDown(UnityEngine.KeyCode.KeypadEnter)) {
+            Vector3[] explosionPosition;
+            CheckExplosionRange.Direction direction = CheckExplosionRange.Direction.Up;
+            Vector3 position = new Vector3(0.0f, 0.0f, 0.0f);
+            Vector3 blockOffset = new Vector3(2.0f, 2.0f, 2.0f); // ブロックの間隔は2
+            Vector3 explosionRange = new Vector3(1.9f, 1.9f, 1.9f); // ぴったりだとギリギリ当たる可能性があるので-0.1する
+            int blockRange = 2; // 2ブロック先まで届く
+            CheckExplosionRange.GetDirectionRange(out explosionPosition, direction, position, blockOffset, explosionRange, blockRange);
+
+            if (explosionPosition != null) {
+                if (explosionPosition.Length == 0) {
+                    Debug.Log("pos empty");
+                }
+                foreach (Vector3 outPosition in explosionPosition) {
+                    Debug.Log(string.Format("pos x={0} y={1} z={2}", outPosition.x, outPosition.y, outPosition.z));
+                }
+            } else {
+                Debug.Log("pos null");
+            }
+
+        }
     }
 }
