e4d1dab333b6a4bccc4167ecbb50419935902e99
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-03-22 18:09:03 +0900

    タイトルシーンを追加、タッチ判定を追加

---
+++ unity_bomberman/Assets/Bomberman/Scripts/Title/TitleSceneScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bomberman {
+    public class TitleSceneScript : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (IsTouch()) {
+                // 画面がタッチされた
+                Debug.Log("touched title");
+            }
+        }
+
+        private bool IsTouch() {
+            int touchCount = UnityEngine.Input.touchCount;
+			for (int i = 0; i < touchCount; ++i) {
+                Touch touch = UnityEngine.Input.GetTouch(i);
+				if (touch.phase == TouchPhase.Ended) {
+					// 実機のタッチ
+                    return true;
+				}
+			}
+
+            if (UnityEngine.Input.GetMouseButtonDown(0)) {
+				// PCのタッチ
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
--- unity_bomberman/Assets/Bomberman/Scripts/UITest/TestButtonScript.cs
+++ unity_bomberman/Assets/Bomberman/Scripts/UITest/TestButtonScript.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestButtonScript : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
+namespace Bomberman {
+    public class TestButtonScript : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
 
-    // Update is called once per frame
-    void Update() {
-        
-    }
+        }
 
-	public void OnButtonClick() {
-        Debug.Log("button");
-	}
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        public void OnButtonClick() {
+            Debug.Log("button");
+        }
+    }
 }
