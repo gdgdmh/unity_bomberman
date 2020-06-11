b6bd213048e2773f393bfdff4f29580006292aef
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-06-11 13:33:03 +0900

    タッチの移動キーとAボタンの判定を取れるように実装

--- unity_bomberman/Assets/Bomberman/Scripts/Game/Hud/GameHudTask.cs
+++ unity_bomberman/Assets/Bomberman/Scripts/Game/Hud/GameHudTask.cs
@@ -6,14 +6,40 @@ using UnityEngine.EventSystems;
 
 public class GameHudTask : MonoBehaviour, Mhl.IButtonLongPressObserverable
 {
+    private static readonly string TouchUiLeftKeyObjectPath = "Canvas/UiLeft";
+    private static readonly string TouchUiRightKeyObjectPath = "Canvas/UiRight";
+    private static readonly string TouchUiUpKeyObjectPath = "Canvas/UiUp";
+    private static readonly string TouchUiDownKeyObjectPath = "Canvas/UiDown";
+
+    private static readonly int MoveLeft = 1;
+    private static readonly int MoveRight = 2;
+    private static readonly int MoveUp = 3;
+    private static readonly int MoveDown = 4;
+    private static readonly int KeyA = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        var g = transform.Find("Canvas/UiLeft");
+        // 長押しは標準ではできないのでオブザーバー登録して取得する
+        // AボタンはUnity標準機能でOnClickに割り当て
+        AddLongPressObserver(TouchUiLeftKeyObjectPath, MoveLeft);
+        AddLongPressObserver(TouchUiRightKeyObjectPath, MoveRight);
+        AddLongPressObserver(TouchUiUpKeyObjectPath, MoveUp);
+        AddLongPressObserver(TouchUiDownKeyObjectPath, MoveDown);
+    }
+
+    /// <summary>
+    /// 長押しのオブザーバー登録
+    /// </summary>
+    /// <param name="objectPath">長押し用のボタンスクリプトがあるオブジェクトのパス</param>
+    /// <param name="parameter">オブザーバーイベント用の固有パラメーター</param>
+    private void AddLongPressObserver(string objectPath, int parameter)
+    {
+        var g = transform.Find(objectPath);
         UnityEngine.Assertions.Assert.IsNotNull(g);
         var script = g.GetComponent<Mhl.LongPressButton>();
         script.AddLongPressObserver(this);
-        script.SetObserverParameter(1);
+        script.SetObserverParameter(parameter);
     }
 
     // Update is called once per frame
@@ -23,25 +49,53 @@ public class GameHudTask : MonoBehaviour, Mhl.IButtonLongPressObserverable
 
     public void OnClick(int number)
     {
-        Debug.Log(number);
+        if (number == KeyA)
+        {
+            Debug.Log("push A");
+        }
     }
 
-    public void OnPointerDownUiRight()
-    {
-    }
-
+    /// <summary>
+    /// 長押し開始
+    /// </summary>
+    /// <param name="eventData">タッチイベントデータ</param>
+    /// <param name="parameter">登録時に割り当てられたパラメーター</param>
     public void StartLongPress(PointerEventData eventData, int parameter)
     {
-        Debug.Log("start" + parameter);
+        //Debug.Log("start" + parameter);
     }
 
+    /// <summary>
+    /// 長押し終了
+    /// </summary>
+    /// <param name="eventData">タッチイベントデータ</param>
+    /// <param name="parameter">登録時に割り当てられたパラメーター</param>
     public void EndLongPress(PointerEventData eventData, int parameter)
     {
-        Debug.Log("end" + parameter);
+        //Debug.Log("end" + parameter);
     }
 
+    /// <summary>
+    /// 長押ししている時に呼ばれる
+    /// </summary>
+    /// <param name="parameter"></param>
     public void LongPress(int parameter)
     {
-        Debug.Log("press" + parameter);
+        if (parameter == MoveLeft)
+        {
+            Debug.Log("press Left");
+        }
+        if (parameter == MoveRight)
+        {
+            Debug.Log("press Right");
+        }
+        if (parameter == MoveUp)
+        {
+            Debug.Log("press Up");
+        }
+        if (parameter == MoveDown)
+        {
+            Debug.Log("press Down");
+        }
     }
 }
