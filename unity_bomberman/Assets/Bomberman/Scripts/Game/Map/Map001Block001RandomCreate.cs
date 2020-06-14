0a79091efca6b5438b3814a4617352d73b87ca4f
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-06-14 23:52:25 +0900

    ブロックのランダム配置で出せるように実装

--- unity_bomberman/Assets/Bomberman/Scripts/Game/Map/Map001Block001RandomCreate.cs
+++ unity_bomberman/Assets/Bomberman/Scripts/Game/Map/Map001Block001RandomCreate.cs
@@ -6,11 +6,16 @@ namespace Bomberman
 {
     public class Map001Block001RandomCreate
     {
+        static readonly int RandMin = 0;
+        static readonly int RandMax = 100;
+        static readonly int CreatePercent = 80;
+
         public void Create()
         {
             Map001DestructableBlockPosition destructableBlockPosition = new Map001DestructableBlockPosition();
             Map001BlockPositionList list = destructableBlockPosition.Get();
             Map001BlockPosition position = new Map001BlockPosition();
+            
 
             // list test
             //Debug.Log(list.Length());
@@ -18,12 +23,34 @@ namespace Bomberman
             //Debug.Log(list.Has(Map001BlockPosition.Position.A4));
 
             InnerBlock001Factory factory = new InnerBlock001Factory();
+            Mhl.IRandIntGeneratable random = new Mhl.RandIntSystem();
+            // 破壊可能ブロックの生成(ランダム)
             int length = list.Length();
             for (int i = 0; i < length; ++i)
             {
+                if (IsRandomBlockCreate(random) == false)
+                {
+                    continue;
+                }
                 Vector3 vector3 = position.ToVector3(list.Get(i));
                 factory.Create(vector3);
             }
+        }
+
+        /// <summary>
+        /// ランダムでブロックを作成するか(乱数で決まる)
+        /// </summary>
+        /// <returns>trueなら作成</returns>
+        private bool IsRandomBlockCreate(Mhl.IRandIntGeneratable random)
+        {
+            random.SetSeed(random.Get());
+            int percent = random.GetRange(RandMin, RandMax);
+            Debug.Log(percent);
+            if (percent >= (RandMax - CreatePercent))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
